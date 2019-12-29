b6aa66181983d1805fb39c8f52a9105a773cec48
BuildTools <szymonwitek02@gmail.com>
2019-12-29 16:50:39 +0100

    Add rotate/shift functions

--- src/main/java/pl/suseu/gameboy360/emulator/instruction/ins/prefixed/PrefixedUtils.java
+++ src/main/java/pl/suseu/gameboy360/emulator/instruction/ins/prefixed/PrefixedUtils.java
@@ -28,5 +28,80 @@ public class PrefixedUtils {
         RegisterUtils.updateReg(gb, r, value);
     }
 
+    private static void updateValue(GBEmulator gb, RegisterUtils.Register r, int val) {
+        if (val == 0)
+            gb.getRegisters().setZeroFlag(1);
+        else
+            gb.getRegisters().setZeroFlag(0);
+        gb.getRegisters().setOperationFlag(0);
+        gb.getRegisters().setHalfCarryFlag(0);
+        RegisterUtils.updateReg(gb, r, val);
+    }
+
+    public static void rotateLeft(GBEmulator gb, RegisterUtils.Register r) {
+        int val = RegisterUtils.getFromReg(gb, r);
+        int carry = gb.getRegisters().getCarryFlag();
+        gb.getRegisters().setCarryFlag((val & 0b10000000) >> 7);
+        rotateLeftAndUpdate(gb, r, val, carry);
+    }
+
+    public static void rotateLeftCarry(GBEmulator gb, RegisterUtils.Register r) {
+        int val = RegisterUtils.getFromReg(gb, r);
+        int bit7 = (val & 0b10000000) >>> 7;
+        gb.getRegisters().setCarryFlag(bit7);
+        rotateLeftAndUpdate(gb, r, val, bit7);
+    }
+
+    private static void rotateLeftAndUpdate(GBEmulator gb, RegisterUtils.Register r, int val, int bit7) {
+        val <<= 1;
+        val &= 0xFF;
+        val |= bit7;
+        updateValue(gb, r, val);
+    }
+
+    public static void rotateRight(GBEmulator gb, RegisterUtils.Register r) {
+        int val = RegisterUtils.getFromReg(gb, r);
+        int carry = gb.getRegisters().getCarryFlag();
+        carry <<= 7;
+        gb.getRegisters().setCarryFlag(val & 1);
+        val >>>= 1;
+        val |= carry;
+        val &= 0xFF;
+        updateValue(gb, r, val);
+    }
+
+    public static void rotateRightCarry(GBEmulator gb, RegisterUtils.Register r) {
+        int val = RegisterUtils.getFromReg(gb, r);
+        int bit0 = val & 1;
+        val >>>= 1;
+        val |= (bit0 << 7);
+        val &= 0xFF;
+        updateValue(gb, r, val);
+    }
+
+    public static void shiftLeft(GBEmulator gb, RegisterUtils.Register r) {
+        int val = RegisterUtils.getFromReg(gb, r);
+        gb.getRegisters().setCarryFlag((val & 0b10000000) >>> 7);
+        val <<= 1;
+        val &= 0xFF;
+        updateValue(gb, r, val);
+    }
+
+    public static void shiftRightA(GBEmulator gb, RegisterUtils.Register r) {
+        int val = RegisterUtils.getFromReg(gb, r);
+        gb.getRegisters().setCarryFlag(val & 1);
+        int bit7 = val & 0b10000000;
+        val &= 0b01111111;
+        val >>>= 1;
+        val |= bit7;
+        updateValue(gb, r, val);
+    }
+
+    public static void shiftRightL(GBEmulator gb, RegisterUtils.Register r) {
+        int val = RegisterUtils.getFromReg(gb, r);
+        gb.getRegisters().setCarryFlag(val & 1);
+        val >>>= 1;
+        updateValue(gb, r, val);
+    }
 
 }
